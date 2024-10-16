9e1adadb32190f4fc191f8fb3a2a102da000af07
Ashutosh <ashutoshchikhaliya@gmail.com>
2024-10-16 14:41:29 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import ChessBoard from './components/ChessBoard';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-800">
-      <h1 className="text-white text-4xl mb-10">Chess Game with Redux</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-zinc-700">
+      <h1 className="text-white text-4xl mb-10">Chess Board with Redux</h1>
       <ChessBoard />
+      <h1 className='text-white'>~ Ashutosh Chikhaliya</h1>
     </div>
   );
 }
